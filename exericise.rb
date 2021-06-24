013b61a6e62e9c4accacf97129d1bf11882384f4
s770am <s770am@gmail.com>
2021-06-24 13:39:09 -0500

    finished all 7

--- exericise.rb
+++ exericise.rb
@@ -1,8 +1,10 @@
 #arrays
+# when i reached 7 i realize that i never made a array for favorite artists :(
 my_favorite_colors = ["red", "blue", "purple", "black" ]
 family_ages = [32, 32, 30, 28, 20]
 coin_flip = ['heads','heads','heads',"tails", 'heads']
 my_favorite_colors_symbols = [:red, :blue, :purple, :black ]
+my_favorite_artists = ["levi robin", "yosef david", "matisyahu"]
 
 # hashes
 three_words = {rapport: "emotianal closeness", Rankle: "to irritate", abase: "to humiliate"}
@@ -97,3 +99,91 @@ end
 # hash_in_array_messenger(my_family_and_age, " is ", " years old")
 
 #4
+
+# my_favorite_colors << ["crimson"]
+# coin_flip << ["tails"]
+# family_ages << [19]
+# my_favorite_colors_symbols << [:crimson]
+
+# three_words[:acclivity] = "sharp upslope of a hill"
+# movies << {name: "the avengers", year: 2012}
+# three_citys << {name: "des plains", population: 58673}
+# my_family_and_age << {name: "dovid", age: 20}
+
+#5
+
+# movies_lists = { 1999 => ["The Matrix", "Star Wars: Episode 1", "The Mummy"],
+# 2009 => ["Avatar", "Star Trek", "District 9"],
+# 2019 => [ "How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]
+# }
+
+# phone_pad = [ [1,2,3],
+# [4,5,6],
+# [7,8,9],
+# ["*",0,"#"]
+# ]
+
+# countries = [ {name: "canada", continent: "north america",is_island: false},
+# {name: "america", continent: "north america",is_island: false},
+# {name: "indonesia", continent: "asia",is_island: true}
+# ]
+
+#6
+# def outputer (message, num) 
+# num.times{puts message}
+# end
+
+# def array_maker_message (array, message, num) 
+#     num.times{array << message}
+#     end
+
+#     def array_maker_num (array, max_num, fill_num = 1)
+# cur = 1
+# cur_fill = 1
+# while cur <= max_num
+# while cur_fill <= fill_num
+#     array << cur
+# cur_fill +=1
+# end
+# cur += 1
+# cur_fill = 1
+# end
+#     end
+
+# outputer("I will not skateboard in the halls", 20)
+
+# dov_array = []
+# array_maker(dov_array,"I will not skateboard in the halls",20)
+
+# puts dov_array
+
+# dov_num_array = []
+# array_maker_num(dov_num_array, 50)
+# p dov_num_array
+
+# dov_num_array = []
+# array_maker_num(dov_num_array, 50, 3)
+# p dov_num_array
+
+#7
+# combined_array = my_favorite_colors.concat(my_favorite_artists).sort
+# puts combined_array
+
+# def music_loving (arr_singers, arr_ages)
+#  arr_singers.each do |singer|
+# arr_ages.each do |age|
+# puts "i love #{singer} #{age}"
+# end
+#  end
+# end
+
+# music_loving(my_favorite_artists, family_ages)
+
+# time_passed_ages = family_ages.map{ |age| age += 1 }
+# puts time_passed_ages
+
+# total_age = family_ages.reduce(0) {|sum, num| sum += num}
+# puts "my familys total age is #{total_age}"
+
+# just_heads = coin_flip.select{|result| result == "heads"}
+# puts just_heads
